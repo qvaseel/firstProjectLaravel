a167d18f2e818ad02ffffdac915eda0e78c4980c
qvaseel <vasilina.makarkina@gmail.com>
2024-09-19 13:01:10 +0500

    create project

---
+++ resources/views/first.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная страница</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Первая страница</h1>
+    
+    <p>{{ $a }} + {{ $b }} = {{ $sum }}</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam nostrum, id quae itaque voluptatibus voluptatum culpa modi necessitatibus, quos deserunt nemo ullam eos! Aspernatur, sint illum. Facere consectetur non dolores.</p>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная страница</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Главная страница</h1>
+    <a href="{{ route('first') }}">На первую страницу</a>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam nostrum, id quae itaque voluptatibus voluptatum culpa modi necessitatibus, quos deserunt nemo ullam eos! Aspernatur, sint illum. Facere consectetur non dolores.</p>
+
+</body>
+</html>
